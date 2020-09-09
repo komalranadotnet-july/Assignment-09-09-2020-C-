b2e6b28bb9eb4049b45a1b5814d395bb633fdda9
komal rana <komal.rana@zensar.com>
2020-09-09 22:52:53 +0530

    Assignment on Array and yield

---
+++ ConArrayDemo/ConArrayDemo/ConYieldKeywordDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConArrayDemo
+{
+    class ConYieldKeywordDemo
+    {
+            static void Main()
+            {
+                // Display powers of 2 up to the exponent of 8:
+                foreach (int i in Power(2, 8))
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+
+            public static System.Collections.Generic.IEnumerable<int> Power(int number, int exponent)
+            {
+                int result = 1;
+
+                for (int i = 0; i < exponent; i++)
+                {
+                    result = result * number;
+                    yield return result;
+                }
+            }
+
+        }
+    }
+
